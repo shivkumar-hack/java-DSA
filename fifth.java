4eebc9a2b9cfb02860b94f5be25afa75277fe6e0
shiv kumar singh <117354199+shivkumar-hack@users.noreply.github.com>
2023-02-02 21:16:32 +0530

    Add files via upload

---
+++ fifth.java
@@ -0,0 +1,36 @@
+ import java.util.*;
+ public class fifth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        int input;
+        do {
+            if(marks>=90 && marks<=100){
+                System.out.println("this is good");
+            }else if (marks>=60 && marks<=89){
+                System.out.println(" this is also good");
+            }else if(marks>=0 && marks<=59){
+                System.out.println("this is good as well");
+            }else{
+                System.out.println("invalid");
+            }
+            System.out.println("want to continue (yes(1) or no(0))");
+             input = sc.nextInt();
+        }while(input==1);
+    }
+        
+        
+        
+    
+             
+        }
+        
+        
+        
+
+       
+    
+        
+    
+    
+
